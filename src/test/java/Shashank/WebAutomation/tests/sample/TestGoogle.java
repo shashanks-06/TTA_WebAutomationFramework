94738c05aacf8d4b0b6fd2487137ee0246802166
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-11-17 23:39:45 +0530

    Initial Commit

--- src/test/java/Shashank/WebAutomation/tests/sample/TestGoogle.java
+++ src/test/java/Shashank/WebAutomation/tests/sample/TestGoogle.java
@@ -1,2 +1,17 @@
-package Shashank.WebAutomation.tests.sample;public class TestGoogle {
+package Shashank.WebAutomation.tests.sample;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestGoogle {
+    @Test
+    public void test() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https:www.google.com");
+        System.out.println(driver.getTitle());
+        Assert.assertEquals(driver.getTitle(), "Google");
+        driver.quit();
+    }
 }
