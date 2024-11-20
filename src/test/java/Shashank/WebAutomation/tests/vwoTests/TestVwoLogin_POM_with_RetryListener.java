7696520a14b5e09952968abc05dbf6e83c355ba5
shashanks-06 <surjekarshashank@gmail.com>
2024-11-20 18:21:27 +0530

    VWO Login Framework with RetryListener

--- src/test/java/Shashank/WebAutomation/tests/vwoTests/TestVwoLogin_POM_with_RetryListener.java
+++ src/test/java/Shashank/WebAutomation/tests/vwoTests/TestVwoLogin_POM_with_RetryListener.java
@@ -5,6 +5,8 @@ import Shashank.WebAutomation.driver.DriverManager;
 import Shashank.WebAutomation.listeners.RetryAnalyzer;
 import Shashank.WebAutomation.pages.pageObjectModel.LoginPage_POM;
 import Shashank.WebAutomation.utils.PropertyReader;
+import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
@@ -16,6 +18,9 @@ public class TestVwoLogin_POM_with_RetryListener extends CommonToAllTest {
 
     private static final Logger logger = LogManager.getLogger(TestVwoLogin_POM_with_RetryListener.class);
 
+
+    @Owner("Shashank")
+    @Description("TC1 - Verify with the invalid login, error message is displayed.")
     @Test(retryAnalyzer = RetryAnalyzer.class)      // To retry the test using RetryListener
     public void test_LoginNegativeVwo(){
         logger.info("Starting the TestCase");
