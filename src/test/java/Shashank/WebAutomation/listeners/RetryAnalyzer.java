f831466f9d14f8dc3b5627f73107d2f89656760e
shashanks-06 <surjekarshashank@gmail.com>
2024-11-20 18:14:21 +0530

    VWO Login Framework with RetryListener

---
+++ src/test/java/Shashank/WebAutomation/listeners/RetryAnalyzer.java
@@ -0,0 +1,19 @@
+package Shashank.WebAutomation.listeners;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryAnalyzer implements IRetryAnalyzer {
+
+    private int retryCount = 0;
+    private static final int maxRetryCount = 3; // Set the max retry count
+
+    @Override
+    public boolean retry(ITestResult result) {
+        if (retryCount < maxRetryCount){
+            retryCount++;
+            return true;    // Retry The Test
+        }
+        return false;
+    }
+}
---
+++ src/test/java/Shashank/WebAutomation/tests/vwoTests/TestVwoLogin_POM_with_RetryListener.java
@@ -0,0 +1,41 @@
+package Shashank.WebAutomation.tests.vwoTests;
+
+import Shashank.WebAutomation.base.CommonToAllTest;
+import Shashank.WebAutomation.driver.DriverManager;
+import Shashank.WebAutomation.listeners.RetryAnalyzer;
+import Shashank.WebAutomation.pages.pageObjectModel.LoginPage_POM;
+import Shashank.WebAutomation.utils.PropertyReader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TestVwoLogin_POM_with_RetryListener extends CommonToAllTest {
+
+    private static final Logger logger = LogManager.getLogger(TestVwoLogin_POM_with_RetryListener.class);
+
+    @Test(retryAnalyzer = RetryAnalyzer.class)      // To retry the test using RetryListener
+    public void test_LoginNegativeVwo(){
+        logger.info("Starting the TestCase");
+        LoginPage_POM loginPagePom = new LoginPage_POM(DriverManager.getDriver());
+
+        logger.info("Opening the URL");
+        String errorMsgText = loginPagePom.loginToVwo_InvalidCreds(
+                PropertyReader.readKey("invalid_username"), PropertyReader.readKey("invalid_password"));
+
+
+        logger.info("Verifying the Error message in the field :-> " +errorMsgText);
+//        AssertJ
+        assertThat(errorMsgText).isNotEmpty().isNotBlank().isNotNull();
+
+//        TestNG Assertions
+        Assert.assertEquals(errorMsgText, PropertyReader.readKey("error_message"));
+
+
+    }
+
+
+
+}
