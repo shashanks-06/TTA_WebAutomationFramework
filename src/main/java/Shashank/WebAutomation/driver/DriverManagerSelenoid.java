5f950f9bd354d28a74ece0678a510c4fe6ddde8d
shashanks-06 <surjekarshashank@gmail.com>
2024-11-20 18:37:27 +0530

    For Parallel Testing

---
+++ src/main/java/Shashank/WebAutomation/driver/DriverManagerSelenoid.java
@@ -0,0 +1,8 @@
+package Shashank.WebAutomation.driver;
+
+public class DriverManagerSelenoid {
+
+    // Upgraded version to support the Multiple Parallel Testing
+    // In the cloud Grids
+
+}
---
+++ src/main/java/Shashank/WebAutomation/driver/DriverManagerTL.java
@@ -0,0 +1,8 @@
+package Shashank.WebAutomation.driver;
+
+public class DriverManagerTL {
+
+    // Upgraded version to support the Multiple Parallel Testing
+    // Thread Local  Support - Run your Testcase Parallel
+
+}
