fa7d9553d79878a25ff3a849b5746a1c4be98394
shashanks-06 <surjekarshashank@gmail.com>
2024-11-21 12:02:23 +0530

    Login to Dashboard to Logout

--- src/test/java/Shashank/WebAutomation/tests/vwoTests/TestVwoLoginPositive_POM.java
+++ src/test/java/Shashank/WebAutomation/tests/vwoTests/TestVwoLoginPositive_POM.java
@@ -5,6 +5,8 @@ import Shashank.WebAutomation.driver.DriverManager;
 import Shashank.WebAutomation.pages.pageObjectModel.DashboardPage_POM;
 import Shashank.WebAutomation.pages.pageObjectModel.LoginPage_POM;
 import Shashank.WebAutomation.utils.PropertyReader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -12,12 +14,17 @@ import static org.assertj.core.api.Assertions.*;
 
 public class TestVwoLoginPositive_POM extends CommonToAllTest {
 
+    private static final Logger logger = LogManager.getLogger(TestVwoLoginPositive_POM.class);
+
     @Test
     public void test_VwoLoginPositive(){
+        logger.info("Starting the TestCase");
+        logger.info("Login to VWO");
         LoginPage_POM loginPagePom = new LoginPage_POM(DriverManager.getDriver());
         loginPagePom.loginToVwo_ValidCreds(
                 PropertyReader.readKey("username"), PropertyReader.readKey("password"));
 
+        logger.info("Getting the name of User on Dashboard");
         DashboardPage_POM dashboardPagePom = new DashboardPage_POM(DriverManager.getDriver());
         String dashboardName =  dashboardPagePom.loggedInUser();
 
@@ -27,11 +34,16 @@ public class TestVwoLoginPositive_POM extends CommonToAllTest {
         Assert.assertEquals(dashboardName, PropertyReader.readKey("expected_username"));
         System.out.println("Expected Username -> " +dashboardName);
 
+        logger.info("Logout the User");
         dashboardPagePom.logoutTheUser();
 
+        logger.info("Getting the name of Logo on LoginPage");
         String vwoLogoName = loginPagePom.verify_backToLoginPage();
         System.out.println("Vwo Logo Name -> " + vwoLogoName);
 
         Assert.assertEquals(vwoLogoName, PropertyReader.readKey("vwo_logo_value"));
+
+        logger.info("Ending the TestCase");
+
     }
 }
