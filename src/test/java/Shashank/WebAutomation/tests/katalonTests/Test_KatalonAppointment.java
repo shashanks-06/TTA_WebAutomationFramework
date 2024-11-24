bc5b3d3ca8c92bf99da47f3af94c2e4fd8e863f7
shashanks-06 <surjekarshashank@gmail.com>
2024-11-24 20:58:48 +0530

    Added Logging using Log4J

--- src/test/java/Shashank/WebAutomation/tests/katalonTests/Test_KatalonAppointment.java
+++ src/test/java/Shashank/WebAutomation/tests/katalonTests/Test_KatalonAppointment.java
@@ -7,6 +7,8 @@ import Shashank.WebAutomation.pages.katalon.Katalon_BookedAppointment_POM;
 import Shashank.WebAutomation.pages.katalon.Katalon_HomePage_POM;
 import Shashank.WebAutomation.pages.katalon.Katalon_LoginPage_POM;
 import Shashank.WebAutomation.utils.PropertyReader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -14,8 +16,14 @@ import static org.assertj.core.api.Assertions.*;
 
 public class Test_KatalonAppointment extends CommonToAllTest {
 
+//    For Logging Purpose using Log4J
+    private static final Logger logger = LogManager.getLogger(Test_KatalonAppointment.class);
+
     @Test
     public void test_appointment(){
+
+        logger.info("Starting the Booking of Hospital Appointment");
+
         Katalon_HomePage_POM katalonHomePagePom = new Katalon_HomePage_POM(DriverManager.getDriver());
         String nameOfHeading = katalonHomePagePom.makeAnAppointment();
         System.out.println(nameOfHeading);
@@ -35,8 +43,6 @@ public class Test_KatalonAppointment extends CommonToAllTest {
 
         Katalon_BookedAppointment_POM katalonBookedAppointmentPom = new Katalon_BookedAppointment_POM(
                 DriverManager.getDriver());
-
-
         String headingOfBookedAppointment = katalonBookedAppointmentPom.getBookedAppointmentHeading();
         assertThat(headingOfBookedAppointment).isEqualTo(PropertyReader.readKey("katalon_appointmentConfirmation"));
         System.out.println(headingOfBookedAppointment);
@@ -63,5 +69,7 @@ public class Test_KatalonAppointment extends CommonToAllTest {
         String addedComment = katalonBookedAppointmentPom.getComment();
         assertThat(addedComment).isEqualTo(PropertyReader.readKey("katalon_expectedComment"));
         System.out.println("Comment : " +  addedComment);
+
+        logger.info("Successfully Done Booking Hospital");
     }
 }
